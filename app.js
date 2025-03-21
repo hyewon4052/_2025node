44d87231abc4eee873485f0736279a21509ef5ff
hyewon4052 <s2311@e-mirim.hs.kr>
2025-03-21 09:16:46 +0900

    params 요청

--- app.js
+++ app.js
@@ -7,19 +7,17 @@ app.use(express.json());
 
 // 기본 라우트를 설정합니다.
 app.get('/', (req, res) => {
-  // 'Hello, World!' 응답을 보냅니다.
-  res.send('Hello, World!');
+    res.send('Hello, World!');
 });
 
-app.get('/swag', (req, res) => {
-  // 'Hello, World!' 응답을 보냅니다.
-  res.send('get swag');
+app.get('/swag/:person', (req, res) => {
+    res.send(req.params.person);
 });
 
 app.post('/swag', (req, res) => {
-  // 'Hello, World!' 응답을 보냅니다.
   res.send(req.body);
 });
+
 
 // 서버가 포트 3000에서 요청을 대기합니다.
 app.listen(3000, () => {
