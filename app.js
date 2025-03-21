66bb25b5302b9c3d0610f245b2b2b172f660fc4f
hyewon4052 <s2311@e-mirim.hs.kr>
2025-03-21 09:56:35 +0900

    Router로 파일을 별도 관리

--- app.js
+++ app.js
@@ -1,22 +1,10 @@
 // Express 모듈을 가져옵니다.
 const express = require('express');
-
-// Express 애플리케이션을 생성합니다.
 const app = express();
+const swagRouter = require('./routes/swag')
+
 app.use(express.json());
-
-// 기본 라우트를 설정합니다.
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
-
-app.get('/swag/:person', (req, res) => {
-    res.send(req.params.person);
-});
-
-app.post('/swag', (req, res) => {
-  res.send(req.body);
-});
+app.use('/swag',swagRouter);
 
 
 // 서버가 포트 3000에서 요청을 대기합니다.
