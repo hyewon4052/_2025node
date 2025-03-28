aaeaeb468b9f7e1c7482c52dc91f53679ab8dbb4
hyewon4052 <s2311@e-mirim.hs.kr>
2025-03-28 10:00:00 +0900

    .env

--- app2.js
+++ app2.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const mysql = require('mysql2');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const db = mysql.createConnection({
+    host : process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+})
 
 travelList = ['뉴욕','서울','파리','도쿄']
 
@@ -10,7 +21,13 @@ app.set('view engine','ejs');
 // path.join을 사용하면 운영체제에 맞추어 경로 구분자(/,\)를 알아서 정해준다.
 app.set('views',path.join(__dirname,'views'));
 
-console.log(path.join(__dirname,'views'));
+db.connect(err=> {
+    if (err) {
+        console.err("mysql 연결 실패 : ",err);
+        return;
+    }
+    console.log("mysql에 연결되었습니다.")
+})
 
 app.get('/', (req, res) => {
     res.render('home');
