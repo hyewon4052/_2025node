f87c5c945db3ba620c0f0c7743752f4116f46097
hyewon4052 <s2311@e-mirim.hs.kr>
2025-04-14 10:53:38 +0900

    라우팅

--- app2.js
+++ app2.js
@@ -1,108 +1,27 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const mysql = require('mysql2');
-const dotenv = require('dotenv');
+const methodOverride = require('method-override');
+const travelRoutes = require('./routes/travel'); 
 
 const port = 3001;
 
-dotenv.config();
 
 
-const db = mysql.createConnection({
-    host : process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-})
-
+app.use(methodOverride('_method'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
+app.use('/travel',travelRoutes);
 app.set('view engine','ejs');
 
 // __dirname : 현재 파일이 속한 절대 경로
 // path.join을 사용하면 운영체제에 맞추어 경로 구분자(/,\)를 알아서 정해준다.
 app.set('views',path.join(__dirname,'views'));
 
-db.connect(err=> {
-    if (err) {
-        console.err("mysql 연결 실패 : ",err);
-        return;
-    }
-    console.log("mysql에 연결되었습니다.")
-})
-
-app.get('/travel',(req,res)=>{
-    const query = 'SELECT id, name FROM travelList';
-    db.query(query, (err, results)=>{
-        if(err) {
-            console.error('데이터베이스 쿼리 실패 : ', err);
-            res.status(500).send('Internal Server Error');
-            return;
-        }
-        const travelList = results;
-        res.render('travel',{travelList})
-    })
-})
-
-app.get('/travel/:id',(req,res)=> {
-    const travelID = req.params.id;
-    const _query = 'SELECT * FROM travelList WHERE id = ?';
-    db.query(_query, [travelID], (err, results)=> {
-        if (err) {
-            console.error('DB 쿼리 실패',err);
-            res.status(500).send('내부 서버 에러');
-            return;
-        }
-        if(results.length === 0) {
-            res.status(404).send('여행지를 찾을 수 없습니다.');
-            return;
-        }
-        const travel = results[0];
-        res.render('travelDetail',{travel});
-    })
-})
-
 app.get('/', (req, res) => {
     res.render('home');
 });
 
-
-app.post('/travel',(req,res)=>{
-    const {name} = req.body;
-    const _query = 'INSERT INTO travelList (name) VALUES (?)';
-    db.query(_query,[name],(err,results)=>{
-        if(err) {
-            console.error('데이터베이스 쿼리 실패 : ', err);
-            res.status(500).send('Internal Server Error');
-            return;
-        }
-        res.redirect('/travel');
-    })
-})
-
-app.put('/travel/:id',(req,res)=> {
-    const travelID = req.params.id;
-    const {name} = req.body;
-    const _query = 'UPDATE travelList SET name = ? WHERE id = ?';
-    db.query(_query, [name, travelID], (err, results)=> {
-        if (err) {
-            console.error('DB 쿼리 실패',err);
-            res.status(500).send('내부 서버 에러');
-            return;
-        }
-        if(results.length === 0) {
-            res.status(404).send('여행지를 찾을 수 없습니다.');
-            return;
-        }
-        res.render('updateSuccess');
-    })
-})
-
-app.get('/add-travel',(req,res)=>{
-    res.render('addTravel');
-})
 
 // use : 모든 method에 대해, 경로가 없으면? : 모든 경로에 대해
 app.use((req,res)=>{
@@ -111,6 +30,6 @@ app.use((req,res)=>{
 
 app.listen(port, () => {
     console.log(`서버가 http://localhost:${port} 에서 실행 중입니다.`);
-    
+
 });
   
---
+++ routes/travel.js
@@ -0,0 +1,114 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../db');
+
+
+router.get('/add',(req,res)=>{
+    res.render('addTravel');
+    
+})
+
+router.get('/',(req,res)=>{
+    const query = 'SELECT id, name FROM travelList';
+    db.query(query, (err, results)=>{
+        if(err) {
+            console.error('데이터베이스 쿼리 실패 : ', err);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+        const travelList = results;
+        res.render('travel',{travelList})
+    })
+})
+
+router.get('/:id',(req,res)=> {
+    const travelID = req.params.id;
+    const _query = 'SELECT * FROM travelList WHERE id = ?';
+    db.query(_query, [travelID], (err, results)=> {
+        if (err) {
+            console.error('DB 쿼리 실패',err);
+            res.status(500).send('내부 서버 에러');
+            return;
+        }
+        if(results.length === 0) {
+            res.status(404).send('여행지를 찾을 수 없습니다.');
+            return;
+        }
+        const travel = results[0];
+        res.render('travelDetail',{travel});
+    })
+})
+
+
+router.post('/',(req,res)=>{
+    const {name} = req.body;
+    const _query = 'INSERT INTO travelList (name) VALUES (?)';
+    db.query(_query,[name],(err,results)=>{
+        if(err) {
+            console.error('데이터베이스 쿼리 실패 : ', err);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+        res.redirect('/travel');
+    })
+})
+
+router.put('/:id',(req,res)=> {
+    const travelID = req.params.id;
+    const {name} = req.body;
+    const _query = 'UPDATE travelList SET name = ? WHERE id = ?';
+    db.query(_query, [name, travelID], (err, results)=> {
+        if (err) {
+            console.error('DB 쿼리 실패',err);
+            res.status(500).send('내부 서버 에러');
+            return;
+        }
+        if(results.length === 0) {
+            res.status(404).send('여행지를 찾을 수 없습니다.');
+            return;
+        }
+        res.render('updateSuccess');
+    })
+})
+
+
+router.get('/:id',(req,res)=> {
+    const travelID = req.params.id;
+    const _query = 'UPDATE travelList * FROM travelList WHERE id = ?';
+    db.query(_query, [travelID], (err, results)=> {
+        if (err) {
+            console.error('DB 쿼리 실패',err);
+            res.status(500).send('내부 서버 에러');
+            return;
+        }
+        if(results.length === 0) {
+            res.status(404).send('여행지를 찾을 수 없습니다.');
+            return;
+        }
+        const travel = results[0];
+        res.render('editSuccess',{travel});
+    })
+})
+
+
+router.delete('/:id',(req,res)=> {
+    const travelID = req.params.id;
+    const _query = 'DELETE FROM travelList WHERE id = ?';
+    db.query(_query, [travelID], (err, results)=> {
+        if (err) {
+            console.error('DB 쿼리 실패',err);
+            res.status(500).send('내부 서버 에러');
+            return;
+        }
+        if(results.length === 0) {
+            res.status(404).send('여행지를 찾을 수 없습니다.');
+            return;
+        }
+        res.render('deleteSuccess');
+    })
+})
+
+
+
+
+module.exports = router;
